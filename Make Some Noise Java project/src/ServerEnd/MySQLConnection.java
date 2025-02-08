4b004ba6480d4f077ccf6ad1a2c965ea7ad689cb
mcm231 <157663112+mcm231@users.noreply.github.com>
2025-02-08 16:57:36 -0500

    basic sql

--- Make Some Noise Java project/src/ServerEnd/MySQLConnection.java
+++ Make Some Noise Java project/src/ServerEnd/MySQLConnection.java
@@ -1,20 +1,28 @@
 package ServerEnd;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MySQLConnection {
     // MySQL Connection URL
-    private static final String URL = "jdbc:mysql://localhost:3306/makeSomeNoiseDatabase";
-    private static final String USER = "remoteUser";
-    private static final String PASSWORD = "remotePassword";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/makeSomeNoiseDatabase";
+    private static final String USER = "appUser";
+    private static final String PASSWORD = "make some noise";
 
     public static void main(String[] args) {
         try {
             // Establish connection
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected to MySQL successfully!");
+
+            String sql = "SELECT * FROM accounts";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "value");
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("Column1: " + rs.getString("column1"));
+            }
 
             // Close connection
             conn.close();
