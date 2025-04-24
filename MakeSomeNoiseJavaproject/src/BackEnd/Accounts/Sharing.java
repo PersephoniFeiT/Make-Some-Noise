05cd5d733a270158ebc43ed3fde9c84f3f19c79e
mcm231 <157663112+mcm231@users.noreply.github.com>
2025-04-24 19:38:33 -0400

    Sharing documentation

--- MakeSomeNoiseJavaproject/src/BackEnd/Accounts/Sharing.java
+++ MakeSomeNoiseJavaproject/src/BackEnd/Accounts/Sharing.java
@@ -6,7 +6,15 @@ import ServerEnd.BasicDatabaseActions;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Maya Malavasi
+ * Provides the specific static methods for any search operations on shared projects in the forum/database. Uses LIKE
+ * instead of an equality check on SQL calls.
+ */
 public class Sharing {
+    /** Searches the database for projects whose titles contain the given substring.
+     * @param title the substring to check for
+     * @return a List of Integer IDs of projects matching the search parameters. */
     public static List<Integer> SearchByTitle(String title){
         List<Integer> projectInfo = new ArrayList<>();
         try {
@@ -17,6 +25,9 @@ public class Sharing {
         return projectInfo;
     }
 
+    /** Searches the database for projects whose usernames contain the given substring.
+     * @param username the substring to check for
+     * @return a List of Integer IDs of projects matching the search parameters. */
     public static List<Integer> SearchByUsername(String username){
         List<Integer> projectInfo = new ArrayList<>();
         try {
@@ -27,7 +38,9 @@ public class Sharing {
         return projectInfo;
     }
 
-    ///TODO
+    /** Searches the database for projects whose tags contain the given substring. Only matches one single tag at a time.
+     * @param tag the tag substring to check for
+     * @return a List of Integer IDs of projects matching the search parameters. */
     public static List<Integer> SearchByTag(String tag){
         List<Integer> projectInfo = new ArrayList<>();
         try {
